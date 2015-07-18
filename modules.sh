4b2f5e698cad2b137cbe87da7cce98fb9ad53078
Karl Mutch <karlmutch@gmail.com>
2015-07-18 17:19:15 -0400

    Getting ready for submodule conversion

---
+++ modules.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+#rm -rf ~/dotfiles/vim/bundle/vim-startify
+#rm -rf ~/dotfiles/vim/bundle/syntastic
+#rm -rf ~/dotfiles/vim/bundle/tabular
+#rm -rf ~/dotfiles/vim/bundle/vim-tmux-navigator
+#rm -rf ~/dotfiles/vim/bundle/vim-nerdtree-tabs
+#rm -rf ~/dotfiles/vim/bundle/vim-maximizer
+#rm -rf ~/dotfiles/vim/bundle/emmet-vim
+#rm -rf ~/dotfiles/vim/bundle/vim-better-whitespace
+#rm -rf ~/dotfiles/vim/bundle/gundo.vim
+#rm -rf ~/dotfiles/vim/bundle/delimitMate
+#rm -rf ~/dotfiles/vim/bundle/vim-instant-markdown
+#rm -rf ~/dotfiles/vim/bundle/vim-airline
+#rm -rf ~/dotfiles/vim/bundle/nerdtree-git-plugin
+#rm -rf ~/dotfiles/vim/bundle/ctrlp.vim
+#rm -rf ~/dotfiles/vim/bundle/go-explorer
+#rm -rf ~/dotfiles/vim/bundle/indentLine
+#rm -rf ~/dotfiles/vim/bundle/vim-fugitive
+#rm -rf ~/dotfiles/vim/bundle/vim-tmux
+#rm -rf ~/dotfiles/vim/bundle/vim-go
+#rm -rf ~/dotfiles/vim/bundle/tagbar
+#rm -rf ~/dotfiles/vim/bundle/vim-gitgutter
+#rm -rf ~/dotfiles/vim/bundle/nerdtree
+
+#exit 0
+
+#vim-startify
+#syntastic
+#tabular
+#vim-tmux-navigator
+#vim-nerdtree-tabs
+#vim-maximizer
+#emmet-vim
+#vim-better-whitespace
+#gundo.vim
+#delimitMate
+#vim-instant-markdown
+#vim-airline
+#nerdtree-git-plugin
+#ctrlp.vim
+#go-explorer
+#indentLine
+#vim-fugitive
+#plugins/vim-tmux
+#vim-go
+#tagbar
+#vim-gitgutter
+#nerdtree
+#
+git submodule add https://github.com/mhinz/vim-startify vim/bundle/vim-startify
+git submodule add https://github.com/scrooloose/syntastic.git vim/bundle/syntastic
+git submodule add https://github.com/godlygeek/tabular.git vim/bundle/tabular
+git submodule add https://github.com/christoomey/vim-tmux-navigator.git vim/bundle/vim-tmux-navigator
+git submodule add https://github.com/jistr/vim-nerdtree-tabs.git vim/bundle/vim-nerdtree-tabs
+git submodule add https://github.com/szw/vim-maximizer.git vim/bundle/vim-maximizer
+git submodule add https://github.com/mattn/emmet-vim.git vim/bundle/emmet-vim
+git submodule add https://github.com/ntpeters/vim-better-whitespace.git vim/bundle/vim-better-whitespace
+git submodule add http://github.com/sjl/gundo.vim.git vim/bundle/gundo.vim
+git submodule add https://github.com/Raimondi/delimitMate.git vim/bundle/delimitMate
+git submodule add https://github.com/suan/vim-instant-markdown.git vim/bundle/vim-instant-markdown
+git submodule add https://github.com/bling/vim-airline vim/bundle/vim-airline
+git submodule add https://github.com/Xuyuanp/nerdtree-git-plugin vim/bundle/nerdtree-git-plugin
+git submodule add https://github.com/kien/ctrlp.vim.git vim/bundle/ctrlp.vim
+git submodule add https://github.com/garyburd/go-explorer.git vim/bundle/go-explorer
+git submodule add https://github.com/Yggdroot/indentLine vim/bundle/indentLine
+git submodule add https://github.com/tpope/vim-fugitive.git vim/bundle/vim-fugitive
+git submodule add git://github.com/tmux-plugins/vim-tmux.git vim/bundle/vim-tmux
+git submodule add https://github.com/fatih/vim-go.git vim/bundle/vim-go
+git submodule add https://github.com/majutsushi/tagbar.git vim/bundle/tagbar
+git submodule add git://github.com/airblade/vim-gitgutter.git vim/bundle/vim-gitgutter
+git submodule add https://github.com/scrooloose/nerdtree.git vim/bundle/nerdtree
