1e9f56e32b67037cf32c82c3caaf3861be860e9e
Karl Mutch <karlmutch@gmail.com>
2018-07-20 09:01:21 -0700

    Added bin targets to the list of files linked to the ~/bin dir

--- bin/k8s-cmds.sh
+++ bin/k8s-cmds.sh
@@ -3,8 +3,5 @@
 DEPLOYMENT=$1
 
 for p in $(kubectl get pods -o jsonpath="{.items[*].metadata.name}"|tr -s '[[:space:]]' '\n' | cut -f1 -d' '|sort|uniq| grep ^${DEPLOYMENT}- | cut -f 1 -d ' '); do
-	echo ---------------------------
-	echo $p
-	echo ---------------------------
-	kubectl exec -it $p -- /bin/df -h
+	echo $p `kubectl exec -it $p -- /bin/df  / -h | tail -n 1`
 done
