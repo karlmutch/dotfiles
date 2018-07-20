9c41c0c95622f78118b588b8518e69479881cd52
Karl Mutch <karlmutch@gmail.com>
2018-07-19 18:43:56 -0700

    added common scripts for k8s

---
+++ bin/k8s-logs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+DEPLOYMENT=$1
+
+for p in $(kubectl get pods | grep ^${DEPLOYMENT}- | cut -f 1 -d ' '); do
+	echo ---------------------------
+	echo $p
+	echo ---------------------------
+	kubectl logs $p
+done
